9e7e71ca5693ea5ff636f0277ba3349a5d967a04
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-10-28 16:02:16 +0600

    Handle Lost Events By Updating Event-Bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,8 +5,12 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
     const event = req.body;
+
+    events.push(event);
 
     // posts service
     axios.post("http://localhost:4000/events", event).catch((err) => {
@@ -25,6 +29,10 @@ app.post("/events", (req, res) => {
         console.log(err.message);
     });
     res.send({ status: "OK" });
+});
+
+app.get("/events", (req, res) => {
+    res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,21 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-
-const posts = {};
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {
-    res.send(posts);
-});
+const posts = {};
 
-app.post("/events", (req, res) => {
-    const { type, data } = req.body;
-
+const handleEvent = (type, data) => {
     if (type === "PostCreated") {
         const { id, title } = data;
 
@@ -40,12 +35,28 @@ app.post("/events", (req, res) => {
         comment.status = status;
         comment.content = content;
     }
+};
 
-    console.log(posts);
+app.get("/posts", (req, res) => {
+    res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+    const { type, data } = req.body;
+
+    handleEvent(type, data);
 
     res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
     console.log("listening on http://localhost:4002");
+
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+        console.log("processing event: " + event.type);
+
+        handleEvent(event.type, event.data);
+    }
 });
