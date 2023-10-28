c52fe7c53aca28516b13fb879650cb8ef2a7b869
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-10-28 15:46:53 +0600

    Add Comment Moderator Service

--- comments/index.js
+++ comments/index.js
@@ -21,7 +21,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
     const comments = commentsByPostId[req.params.id] || [];
 
-    comments.push({ id: commentId, content });
+    comments.push({ id: commentId, content, status: "pending" });
 
     commentsByPostId[req.params.id] = comments;
 
@@ -31,14 +31,37 @@ app.post("/posts/:id/comments", async (req, res) => {
             id: commentId,
             content,
             postId: req.params.id,
+            status: "pending",
         },
     });
 
     res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
+app.post("/events", async (req, res) => {
     console.log("Recieved Event: ", req.body.type);
+
+    const { type, data } = req.body;
+
+    if (type === "CommentModerated") {
+        const { postId, id, status, content } = data;
+        const comments = commentsByPostId[postId];
+
+        const comment = comments.find((comment) => {
+            return comment.id === id;
+        });
+        comment.status = status;
+
+        await axios.post("http://localhost:4005/events", {
+            type: "CommentUpdated",
+            data: {
+                id,
+                content,
+                postId,
+                status,
+            },
+        });
+    }
 
     res.send({});
 });
--- event-bus/index.js
+++ event-bus/index.js
@@ -8,13 +8,20 @@ app.use(bodyParser.json());
 app.post("/events", (req, res) => {
     const event = req.body;
 
+    // posts service
     axios.post("http://localhost:4000/events", event).catch((err) => {
         console.log(err.message);
     });
+    // comments service
     axios.post("http://localhost:4001/events", event).catch((err) => {
         console.log(err.message);
     });
+    // query service
     axios.post("http://localhost:4002/events", event).catch((err) => {
+        console.log(err.message);
+    });
+    // moderation service
+    axios.post("http://localhost:4003/events", event).catch((err) => {
         console.log(err.message);
     });
     res.send({ status: "OK" });
