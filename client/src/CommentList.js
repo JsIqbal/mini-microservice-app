a1007f6ea73e449cc1089c89c344bcb3a1bae4c3
Iqbal Hossain <javascriptiqbal@gmail.com>
2023-10-28 13:43:36 +0600

    Add My Project To Repo

---
+++ client/src/CommentList.js
@@ -0,0 +1,9 @@
+const CommentList = ({ comments }) => {
+    const renderedComments = comments.map((comment) => {
+        return <li key={comment.id}>{comment.content}</li>;
+    });
+
+    return <ul>{renderedComments}</ul>;
+};
+
+export default CommentList;
