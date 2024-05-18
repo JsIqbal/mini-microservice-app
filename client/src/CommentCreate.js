7ab2e42641156d7ff26892dfbb869caba40c8651
JsIqbal <javascriptiqbal@gmail.com>
2024-05-18 12:47:54 +0600

    Implement Like Feature

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -2,33 +2,33 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const CommentCreate = ({ postId }) => {
-  const [content, setContent] = useState("");
+    const [content, setContent] = useState("");
 
-  const onSubmit = async (event) => {
-    event.preventDefault();
+    const onSubmit = async (event) => {
+        event.preventDefault();
 
-    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
-      content,
-    });
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content,
+        });
 
-    setContent("");
-  };
+        setContent("");
+    };
 
-  return (
-    <div>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label>New Comment</label>
-          <input
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-            className="form-control"
-          />
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div className="form-group">
+                    <label>New Comment</label>
+                    <input
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                        className="form-control"
+                    />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
         </div>
-        <button className="btn btn-primary">Submit</button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default CommentCreate;
